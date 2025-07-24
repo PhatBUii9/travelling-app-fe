23dca90e4fbd7ab527a1950f983a5b2fbf7cb7ea
Frank Bui <tanphat.bui2512@gmail.com>
2025-07-24 22:38:45 +1000

    feat(trip-review): implement TripCityBlock with all trip sections

--- components/trip/trip-review/TripCityBlock.tsx
+++ components/trip/trip-review/TripCityBlock.tsx
@@ -1,12 +1,17 @@
 import { mockActivities } from "@/data/mockActivities";
+import { mockRestaurants } from "@/data/mockRestaurants";
 import {
   View,
   Text,
   TouchableOpacity,
   ListRenderItem,
   Image,
+  Alert,
 } from "react-native";
 import { FlatList } from "react-native-actions-sheet";
+import MaterialIcon from "react-native-vector-icons/MaterialIcons";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { mockAccommodations } from "@/data/mockAccommodations";
 
 type TripCityBlockProps = {
   cityName: string;
@@ -49,10 +54,10 @@ const TripSection = <T,>({
         <FlatList
           data={data}
           keyExtractor={(item) => (item as any).id}
-          ItemSeparatorComponent={() => <View className="h-3" />}
+          ItemSeparatorComponent={() => <View className="h-2" />}
           showsVerticalScrollIndicator={false}
           ListHeaderComponent={() => (
-            <View className="flex-row justify-between mb-2">
+            <View className="flex-row justify-between mb-1">
               <Text className="text-lg font-JakartaSemiBold">
                 {sectionTitle}
               </Text>
@@ -68,10 +73,15 @@ const TripSection = <T,>({
             </View>
           )}
           renderItem={renderItem}
+          ListFooterComponent={() => <View className="mb-3" />}
         />
       </View>
     </View>
   );
+};
+
+const Divider = () => {
+  return <View className="h-1 w-full bg-gray-200 rounded-full my-2" />;
 };
 
 const TripCityBlock = ({
@@ -87,39 +97,199 @@ const TripCityBlock = ({
 
   const handleAdd = () => {};
 
+  const handleDelete = () => {
+    Alert.alert("Remove City", "Do you want to remove this city?", [
+      {
+        text: "Cancel",
+        onPress: () => console.log("Cancel Pressed"),
+        style: "cancel",
+      },
+      {
+        text: "OK",
+        style: "destructive",
+        onPress: () => {
+          console.log("Ok Pressed");
+        },
+      },
+    ]);
+  };
+
   const fullActivities = activities
     .map((id) => mockActivities.find((a) => a.id === id))
     .filter(Boolean);
 
+  const fullRestaurants = restaurants!
+    .map((id) => mockRestaurants.find((a) => a.id === id))
+    .filter(Boolean);
+
+  const fullAccommodations = accommodations!
+    .map((id) => mockAccommodations.find((a) => a.id === id))
+    .filter(Boolean);
+
   return (
-    <View className="">
-      <Text className="text-heading-md font-JakartaBold">{cityName}</Text>
-      <Text className="text-md font-JakartaSemiBold text-secondary-500 mb-5">
-        {formatDate(startDate)} - {formatDate(endDate)}
-      </Text>
+    <View className="bg-white px-4 pb-4 pt-2 rounded-3xl mb-5">
+      <View className="flex-row items-center justify-between mb-1">
+        <Text className="text-heading-md font-JakartaBold">{cityName}</Text>
+        <TouchableOpacity
+          onPress={handleDelete}
+          accessibilityLabel="Remove city"
+        >
+          <Icon name="trash" size={24} color="#FF0000" />
+        </TouchableOpacity>
+      </View>
+      <View className="flex-row items-center justify-between mb-3">
+        <Text className="text-base font-JakartaSemiBold text-secondary-500">
+          {country}
+        </Text>
+        <Text className="text-md font-JakartaSemiBold text-secondary-600">
+          {formatDate(startDate)} - {formatDate(endDate)}
+        </Text>
+      </View>
+
+      {/* Activity section */}
       <TripSection
         sectionTitle="Activities"
         data={fullActivities}
         renderItem={({ item }: { item: any }) => (
-          <View className="flex-row items-center mb-2 ml-2">
-            <Image source={item.image} className="w-14 h-14 rounded-md mr-3" />
+          <View className="flex-row items-center p-2 bg-gray-50 rounded-xl">
+            {!!item.image ? (
+              <Image
+                source={item.image}
+                className="w-10 h-10 rounded-md mr-3 ml-2"
+              />
+            ) : (
+              <View className="w-10 h-10 mr-3 items-center justify-between ml-2">
+                <MaterialIcon name="place" size={40} color="#000" />
+              </View>
+            )}
             <View className="flex-1">
               <Text
-                className="font-JakartaSemiBold text-md"
+                className="font-JakartaSemiBold text-base"
                 numberOfLines={1}
                 ellipsizeMode="tail"
               >
                 {item.name}
               </Text>
-              <Text className="text-sm text-secondary-600">
-                {item.category}
-              </Text>
+              <View className="flex-row items-center">
+                <Icon
+                  name="tag"
+                  size={12}
+                  color="#0286FF"
+                  testID="category-icon"
+                />
+                <Text className="text-xs font-JakartaMedium text-secondary-600 ml-1">
+                  {item.category}
+                </Text>
+              </View>
             </View>
           </View>
         )}
         onEdit={handleEdit}
         onAdd={handleAdd}
-        isListEmpty={activities.length === 0}
+        isListEmpty={fullActivities.length === 0}
+      />
+
+      <Divider />
+
+      {/* Restaurant section */}
+      <TripSection
+        sectionTitle="Restaurants"
+        data={fullRestaurants}
+        renderItem={({ item }: { item: any }) => (
+          <View className="flex-row items-center p-2 bg-gray-50 rounded-xl">
+            {!!item.image ? (
+              <Image
+                source={item.image}
+                className="w-10 h-10 rounded-md mr-3 ml-2"
+              />
+            ) : (
+              <View className="w-10 h-10 mr-3 items-center justify-between ml-2">
+                <MaterialIcon name="restaurant" size={40} color="#000" />
+              </View>
+            )}
+            <View className="flex-1">
+              <Text
+                className="font-JakartaSemiBold text-base"
+                numberOfLines={1}
+                ellipsizeMode="tail"
+              >
+                {item.name}
+              </Text>
+              <View className="flex-row items-center justify-between w-full">
+                <View className="flex-row items-center">
+                  <Text className="text-xs font-JakartaMedium text-secondary-600 mr-1">
+                    {item.rating}
+                  </Text>
+                  <Icon
+                    name="star"
+                    size={14}
+                    color="#22C55E"
+                    testID="rating-icon"
+                  />
+                </View>
+                <View className="flex-row items-center">
+                  <MaterialIcon name="food-bank" size={16} color="#000" />
+                  <Text className="text-xs font-JakartaSemiBold text-secondary-600 mr-3 ml-1">
+                    {item.cuisine}
+                  </Text>
+                </View>
+              </View>
+            </View>
+          </View>
+        )}
+        onEdit={handleEdit}
+        onAdd={handleAdd}
+        isListEmpty={fullRestaurants.length === 0}
+      />
+
+      <Divider />
+
+      {/* Accommodation section */}
+      <TripSection
+        sectionTitle="Accommodations"
+        data={fullAccommodations}
+        renderItem={({ item }: { item: any }) => (
+          <View className="flex-row items-center p-2 bg-gray-50 rounded-xl">
+            {!!item.image ? (
+              <Image
+                source={item.image}
+                className="w-10 h-10 rounded-md mr-3 ml-2"
+              />
+            ) : (
+              <View className="w-10 h-10 mr-3 items-center justify-between ml-2">
+                <MaterialIcon name="hotel" size={40} color="#000" />
+              </View>
+            )}
+            <View className="flex-1">
+              <Text
+                className="font-JakartaSemiBold text-base"
+                numberOfLines={1}
+                ellipsizeMode="tail"
+              >
+                {item.name}
+              </Text>
+              <View className="flex-row justify-between items-center w-full">
+                <View className="flex-row items-center">
+                  <Text className="text-xs font-JakartaMedium text-secondary-600 mr-1">
+                    {item.rating}
+                  </Text>
+                  <Icon
+                    name="star"
+                    size={14}
+                    color="#22C55E"
+                    testID="rating-icon"
+                  />
+                </View>
+                <Text className="text-xs font-JakartaSemiBold text-secondary-600 mr-3">
+                  ${item.pricePerNight}/night
+                </Text>
+              </View>
+            </View>
+          </View>
+        )}
+        onEdit={handleEdit}
+        onAdd={handleAdd}
+        isListEmpty={fullAccommodations.length === 0}
       />
     </View>
   );
